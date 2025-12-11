e9dd02e7c453fe16178af8594a72e4d995eb5f29
aMOBSTERgail <349191643@gapps.yrdsb.ca>
2025-12-11 14:18:37 -0500

    Created MyWorld background

--- MyWorld.java
+++ MyWorld.java
@@ -3,5 +3,9 @@ import greenfoot.*;
 public class MyWorld extends World {
     public MyWorld() {
         super(600, 400, 1);
+        
+        GreenfootImage background = new GreenfootImage();
+        background.scale(getWidth(), getHeight());
+        setBackground(background);
     }
 }
