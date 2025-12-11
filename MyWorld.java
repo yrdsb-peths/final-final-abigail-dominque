bff1002936c42b3483e4dad1665891f2e46c9fd8
aMOBSTERgail <349191643@gapps.yrdsb.ca>
2025-12-11 14:23:04 -0500

    Updated background image

--- MyWorld.java
+++ MyWorld.java
@@ -4,7 +4,7 @@ public class MyWorld extends World {
     public MyWorld() {
         super(600, 400, 1);
         
-        GreenfootImage background = new GreenfootImage();
+        GreenfootImage background = new GreenfootImage("background.png");
         background.scale(getWidth(), getHeight());
         setBackground(background);
     }
