d700128566e916b097d94f781ec26d2326fd3a0f
aMOBSTERgail <349191643@gapps.yrdsb.ca>
2025-12-11 14:34:34 -0500

    Created Player placeholder

--- Player.java
+++ Player.java
@@ -12,6 +12,10 @@ public class Player extends Actor
      * Act - do whatever the Player wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    public player()
+    {
+        setImage("player.png");
+    }
     public void act()
     {
         // Add your action code here.
