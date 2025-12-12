2fa2c56c8808f6ef25131b7472853b9982a771d1
aMOBSTERgail <349191643@gapps.yrdsb.ca>
2025-12-12 14:11:15 -0500

    Added MushroomHut to MushroomWorld

--- MushroomWorld.java
+++ MushroomWorld.java
@@ -4,11 +4,17 @@ public class MushroomWorld extends World {
     public MushroomWorld() {
         super(600, 600, 1);
         
+        // Sets background
         GreenfootImage background = new GreenfootImage("background.png");
         background.scale(getWidth(), getHeight());
         setBackground(background);
         
+        // Add player
         Player player = new Player();
         addObject(player, 300, 200);
+        
+        // Adds item
+        MushroomHut hut = new MushroomHut();
+        addObject(hut, 400, 300);
     }
 }
