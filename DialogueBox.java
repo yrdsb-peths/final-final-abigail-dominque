95c55f61c52eb367a1de7e634b873995e94bed0a
doMOBSTERnique <348787912@gapps.yrdsb.ca>
2025-12-12 14:42:55 -0500

    Deleted failed dialogue

--- DialogueBox.java
+++ DialogueBox.java
@@ -1,4 +1,5 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import java.util.ArrayList;
 
 /**
  * Write a description of class DialogueBox here.
@@ -12,28 +13,9 @@ public class DialogueBox extends Actor
      * Act - do whatever the DialogueBox wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
-    public DialogueBox(String dialogue)
-    {
-        updateImage(dialogue);
-    }
     
-    private void updateImage(String dialogue)
-    {
-        GreenfootImage pic = new GreenfootImage(500, 120);
-        pic.setColor(new Color(255, 255, 255, 220));
-        pic.fillRect(0, 0, 500, 120);
-        
-        pic.setColor(Color.BLACK);
-        pic.setFont(new Font("Arial", 24));
-        pic.drawString(dialogue, 20, 60);
-        
-        setImage(pic);
-    }
+    private ArrayList<String> lines;
     
-    private void setDialogue(String dialogue)
-    {
-        updateImage(dialogue);
-    }
     
     public void act()
     {
