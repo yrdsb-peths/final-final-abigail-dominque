eeb2f22eede7db93898b0cf0c48b1f9a1f83c6ab
doMOBSTERnique <348787912@gapps.yrdsb.ca>
2025-12-12 14:10:36 -0500

    made dialogueworld

---
+++ DialogueWorld.java
@@ -0,0 +1,21 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class DialogueWorld here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class DialogueWorld extends World
+{
+
+    /**
+     * Constructor for objects of class DialogueWorld.
+     * 
+     */
+    public DialogueWorld()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 600, 1); 
+    }
+}
