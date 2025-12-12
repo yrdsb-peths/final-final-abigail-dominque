a09052a7e1ca1b5459ab9f29084be038d3ca861b
aMOBSTERgail <349191643@gapps.yrdsb.ca>
2025-12-12 14:05:22 -0500

    Setted MushroomHut's image

--- MushroomHut.java
+++ MushroomHut.java
@@ -12,6 +12,13 @@ public class MushroomHut extends Actor
      * Act - do whatever the MushroomHut wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    
+    // Sets the mushroom hut's image
+    public MushroomHut()
+    {
+        setImage("mushroomhut.png");
+    }
+    
     public void act()
     {
         // Add your action code here.
