cf3044ea83b5a863c08709535e5a9f7171ea0977
doMOBSTERnique <348787912@gapps.yrdsb.ca>
2025-12-15 14:16:24 -0500

    Scaled Big Mushroom

--- MushroomBigNPC.java
+++ MushroomBigNPC.java
@@ -17,10 +17,17 @@ public class MushroomBigNPC extends Actor
     
     public MushroomBigNPC()
     {
+         //Loads and scales new images
          image1 = new GreenfootImage("mushroom_big_image1.png");
          image2 = new GreenfootImage("mushroom_big_image2.png");
          image3 = new GreenfootImage("mushroom_big_image3.png");
-        
+         
+         image1.scale(BIG_HEIGHT, BIG_WIDTH);
+         image2.scale(BIG_HEIGHT, BIG_WIDTH);
+         image3.scale(BIG_HEIGHT, BIG_WIDTH);
+         
+         //Sets intial image
+         setImage(image1);
     }
     
     public void act()
