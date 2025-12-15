f3517076b4b36ad2a9dcc54805d4c9183fd9aab7
aMOBSTERgail <349191643@gapps.yrdsb.ca>
2025-12-15 14:47:36 -0500

    Finished the instructions collision for player and npc

--- Instructions.java
+++ Instructions.java
@@ -2,24 +2,31 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 public class Instructions extends World
 {
+    private int returnX;
+    private int returnY;
 
-    /**
-     * Constructor for objects of class Instructions.
-     * 
-     */
-    public Instructions()
-    {    
-        // Create a new world with 600x600 cells with a cell size of 1x1 pixels.
-        super(1000, 700, 1); 
-        
-        MushroomBigNPC mushroom = new MushroomBigNPC();
-        addObject(mushroom, 500, 350);
-        
-        String [] text1 = {
-            "Welcome to Fairyland!"
+    public Instructions(int playerX, int playerY)
+    {
+        super(1000, 700, 1);
+
+        returnX = playerX;
+        returnY = playerY;
+
+        String[] lines = {
+            "Welcome to Fairyland!",
+            "Use arrow keys to move.",
+            "Talk to characters to learn things."
         };
-        
-        DialogueBox txt1 = new DialogueBox(text1);
-        addObject(txt1, getWidth() / 2, 500);
+
+        addObject(new DialogueBox(lines), getWidth() / 2, 300);
+    }
+
+    public void act()
+    {
+        // When dialogue finishes, go back
+        if (getObjects(DialogueBox.class).isEmpty())
+        {
+            Greenfoot.setWorld(new MushroomWorld(returnX, returnY));
+        }
     }
 }
