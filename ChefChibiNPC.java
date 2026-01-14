5a849e69a4385cf52c9aed585f9bffad782b4d7a
doMOBSTERnique <348787912@gapps.yrdsb.ca>
2026-01-13 20:36:50 -0500

    chef chibi update

--- ChefChibiNPC.java
+++ ChefChibiNPC.java
@@ -9,8 +9,8 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class ChefChibiNPC extends Actor
 {
     // Sets the players height and width
-    private static final int WIDTH = 80;
-    private static final int HEIGHT = 80;
+    private static final int WIDTH = 250;
+    private static final int HEIGHT = 250;
     
     private GreenfootImage idle; 
     
